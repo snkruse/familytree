ad127a8efc5b27ecb99baa56236412cf3f350a3d
snkruse <sethkruse@gmail.com>
2016-06-14 23:20:08 -0700

    scanner being used

--- FamilyTree.java
+++ FamilyTree.java
@@ -2,6 +2,7 @@
 public class FamilyTree {
 	public static void main(String[] args){
 		System.out.println("Begin Program");
-		
+		Person p1 = new Person();
+		p1.getPerson();
 	}
 }
--- Person.java
+++ Person.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+import java.util.*;
+
 
 public class Person {
 	String name;
 	int age;
 	
+	//Default Constructor
+	public Person(){
+		this.name = null;
+		this.age = 0; 
+	}
 	
+   // This is the constructor of the class Employee
+   public Person(String name){
+      this.name = name;
+   }
+   
+   public Person(String name, int age){
+	   this.name = name;
+	   this.age = age;
+   }
+   
+   public void getPerson(){
+	// Initiate a new Scanner
+       Scanner userInputScanner = new Scanner(System.in);
+       System.out.print("\nWhat is your name? ");
+       String name = userInputScanner.nextLine();
+       System.out.println(name);
+       userInputScanner.close();
+   }
+
 }
