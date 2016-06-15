9a343facee0898691db72dd348e5c47659b7b194
snkruse <sethkruse@gmail.com>
2016-06-15 00:04:13 -0700

    added copy constructor to Person.java

--- FamilyTree.java
+++ FamilyTree.java
@@ -3,6 +3,10 @@ public class FamilyTree {
 	public static void main(String[] args){
 		System.out.println("Begin Program");
 		Person p1 = new Person();
-		p1.getPerson();
+		Person p2 = p1.getPerson();
+		Person p3 = new Person(p2);
+		
+		p2.print(p2);
+		p3.print(p3);
 	}
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.*;
+import java.util.InputMismatchException;
 
 
 public class Person {
@@ -12,23 +12,52 @@ public class Person {
 		this.age = 0; 
 	}
 	
-   // This is the constructor of the class Employee
+   // Constructor for only 'name'
    public Person(String name){
       this.name = name;
+      this.age = 0;
    }
    
+   //Constructor for 'name' & 'int'
    public Person(String name, int age){
 	   this.name = name;
 	   this.age = age;
    }
    
-   public void getPerson(){
+   //Copy Constructor 
+   public Person(Person another) {
+	    this.name = another.name;
+	    this.age  = another.age;
+	  }
+   
+   
+   public void print(Person p){
+	   System.out.println(name + " " + age);
+   }
+   
+   public Person getPerson(){
 	// Initiate a new Scanner
        Scanner userInputScanner = new Scanner(System.in);
-       System.out.print("\nWhat is your name? ");
-       String name = userInputScanner.nextLine();
-       System.out.println(name);
+       System.out.println("What is your name? ");
+       String tempName = userInputScanner.nextLine();
+      
+       boolean validInput = false;
+       while(!validInput){
+    	   System.out.println("What is thier age");
+       try{
+    	   age = userInputScanner.nextInt();
+    	   validInput = true;
+       }catch(InputMismatchException e){
+    	   validInput = false;
+    	   userInputScanner.nextLine();
+       }
+       }
        userInputScanner.close();
+       System.out.println(tempName + " " + age);
+       Person newPerson = new Person(tempName,age);
+       return newPerson;
+       
+       
    }
 
 }
